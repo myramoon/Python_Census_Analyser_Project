b4f2bd661fc353bcf37d5f09c9d4a655f67aa4fe
anamfazal <anamfazal94@gmail.com>
2020-11-21 01:23:37 +0530

    Refactored: functionality code to raise exception if wrong file path is given

---
+++ main/CensusAnalyserException.py
@@ -0,0 +1,9 @@
+class CensusAnalyserError(Exception):
+    def __init__(self, message):
+        """
+        :param message:custom message to be displayed
+        """
+        super().__init__(message)
+
+class WrongFilePathError(CensusAnalyserError):
+    pass
--- main/census_analyser.py
+++ main/census_analyser.py
@@ -1,20 +1,24 @@
 import csv
 import logging
+from main.CensusAnalyserException import WrongFilePathError
+
 
 class CensusAnalyser:
-
     logging.basicConfig(filename='census_analyser.log', level = logging.DEBUG , format = '%(name)s | %(levelname)s | %(asctime)s | %(message)s' )
-
 
     def load_census_data(self , csv_file_path):
         """
         :param csv_file_path:path containing csv file to be opened
         :return: number of records excluding header in csv file
         """
-        with open(csv_file_path , 'r') as census_data:
-            csv_reader = csv.reader(census_data)
-            next(csv_reader)
-            count = (len(list(csv_reader)))
-            logging.debug('Number of records: {}'.format(count))
-            return count
+        try:
+            with open(csv_file_path , 'r') as census_data:
+                csv_reader = csv.reader(census_data)
+                next(csv_reader)
+                count = (len(list(csv_reader)))
+                logging.debug('Number of records: {}'.format(count))
+        except FileNotFoundError:
+            logging.exception('Exception occurred due to wrong file path')
+            raise WrongFilePathError("Please enter correct file path")
 
+        return count
