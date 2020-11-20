b34c58253004699cb578d37a51e9f6d6d730be92
anamfazal <anamfazal94@gmail.com>
2020-11-21 01:50:45 +0530

    Refactored: test code to verify exception when wrong file extension is given

--- test/test_census_analyser.py
+++ test/test_census_analyser.py
@@ -1,8 +1,9 @@
 import pytest
-from main.CensusAnalyserException import WrongFilePathError
+from main.CensusAnalyserException import WrongFilePathError, WrongFileExtensionError
 
 STATE_CENSUS_DATA_PATH = '/Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.csv'
 WRONG_STATE_CENSUS_DATA_PATH = 'Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.csv'
+WRONG_FILE_EXTENSION = 'Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.json'
 
 def test_count_records_in_census_data_csv_file(instance_of_census_analyser):
     count_of_records = instance_of_census_analyser.load_census_data(STATE_CENSUS_DATA_PATH)
@@ -11,5 +12,9 @@ def test_count_records_in_census_data_csv_file(instance_of_census_analyser):
 def test_given_wrong_file_path_should_raise_exception(instance_of_census_analyser):
     with pytest.raises(WrongFilePathError):
         instance_of_census_analyser.load_census_data(WRONG_STATE_CENSUS_DATA_PATH)
+
+def test_given_wrong_file_extension_should_raise_exception(instance_of_census_analyser):
+    with pytest.raises(WrongFileExtensionError):
+        instance_of_census_analyser.load_census_data(WRONG_FILE_EXTENSION)
 
 
