f569f4fbb3d7fe82e198aee0795832668b81a834
anamfazal <anamfazal94@gmail.com>
2020-11-21 03:53:24 +0530

    [myramoon]Added: test code to verify count of records for state code data csv file

--- test/test_census_analyser.py
+++ test/test_census_analyser.py
@@ -3,7 +3,10 @@ from main.CensusAnalyserException import WrongFilePathError, WrongFileExtensionE
 
 STATE_CENSUS_DATA_PATH = '/Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.csv'
 WRONG_STATE_CENSUS_DATA_PATH = 'Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.csv'
-WRONG_FILE_EXTENSION = 'Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.json'
+WRONG_FILE_EXTENSION = '/Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCensusData.json'
+STATE_CODE_PATH = '/Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCode.csv'
+WRONG_STATE_CODE_DATA_PATH = 'Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCode.csv'
+WRONG_STATE_CODE_FILE_EXTENSION = '/Users/nusrat/PycharmProjects/PythonProjects/census_analyser_project/main/IndiaStateCode.json'
 
 def test_count_records_in_census_data_csv_file(instance_of_census_analyser):
     count_of_records = instance_of_census_analyser.load_census_data(STATE_CENSUS_DATA_PATH)
@@ -17,4 +20,14 @@ def test_given_wrong_file_extension_should_raise_exception(instance_of_census_an
     with pytest.raises(WrongFileExtensionError):
         instance_of_census_analyser.load_census_data(WRONG_FILE_EXTENSION)
 
+def test_count_records_in_state_code_data_csv_file(instance_of_census_analyser):
+    count_of_records = instance_of_census_analyser.load_census_data(STATE_CODE_PATH)
+    assert count_of_records == 37
 
+def test_given_wrong_state_code_file_path_should_raise_exception(instance_of_census_analyser):
+    with pytest.raises(WrongFilePathError):
+        instance_of_census_analyser.load_census_data(WRONG_STATE_CENSUS_DATA_PATH)
+
+def test_given_wrong_state_code_file_extension_should_raise_exception(instance_of_census_analyser):
+    with pytest.raises(WrongFileExtensionError):
+        instance_of_census_analyser.load_census_data(WRONG_FILE_EXTENSION)
