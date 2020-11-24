76fc4562e3df0819809cd720695f82c1116a60ac
anamfazal <anamfazal94@gmail.com>
2020-11-24 12:06:39 +0530

    [myramoon]Refactored: exception class to include docstrings

--- main/CensusAnalyserException.py
+++ main/CensusAnalyserException.py
@@ -1,7 +1,9 @@
 import enum
 
 class ExceptionType(enum.Enum):
-
+    """
+    :param: enums associated to possible errors in census_analyser
+    """
     FilePathError = "Please give proper file path"
     FileExtensionError = "Please check file extension"
     ImproperHeader = "Please check csv file's header"
@@ -11,7 +13,7 @@ class CensusAnalyserError(Exception):
 
     def __init__(self, *args):
         """
-        :param message:custom message to be displayed
+        :param args: type and message associated to exception
         """
         self.type = args[0]
         self.message = args[1]
